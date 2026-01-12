d2546427995d92f22f5275a3a5fca6b9435c719e
Nicole Cristina <nicolecristina.uv@gmail.com>
2026-01-12 16:48:05 -0300

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,64 +1,146 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client';
+
+import "./styles.css";
+import { useState } from "react";
+import { FaCheck, FaRegCopy } from "react-icons/fa";
 
 export default function Home() {
+  const [offsetX, setOffsetX] = useState(0);
+  const [offsetY, setOffsetY] = useState(10);
+  const [blur, setBlur] = useState(15);
+  const [spread, setSpread] = useState(-3);
+  const [color, setColor] = useState("#000000");
+  const [opacity, setOpacity] = useState(0.1);
+  const [inset, setInset] = useState(false);
+  const [copied, setCopied] = useState(false);
+  const rgbaColor = () => {
+    const r = parseInt(color.slice(1, 3), 16);
+    const g = parseInt(color.slice(3, 5), 16);
+    const b = parseInt(color.slice(5, 7), 16);
+
+    return `rgba(${r}, ${g}, ${b}, ${opacity})`;
+  };
+
+  const copyToClipboard = () => {
+    const code = `box-shadow: ${inset ? "inset " : ""}${offsetX}px ${offsetY}px ${blur}px ${spread}px ${rgbaColor()}`;
+    navigator.clipboard.writeText(code);
+    setCopied(true);
+    setTimeout(() => setCopied(false), 2000);
+  }
+
+  
+
+  const boxShadowValue = `${inset ? "inset " : ""}${offsetX}px ${offsetY}px ${blur}px ${spread}px ${rgbaColor()}`;
+
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className={styles.intro}>
-          <h1>To get started, edit the page.js file.</h1>
-          <p>
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
+    <div>
+      <h1>Box Shadow Generator</h1>
+
+      <main className="container">
+        <div className="controls">
+          <div className="control-group">
+            <label htmlFor="offsetX">Horizontal displacement</label>
+            <input
+              type="range"
+              id="offsetX"
+              min="-100"
+              max="100"
+              value={offsetX}
+              onChange={(e) => setOffsetX(Number(e.target.value))}
             />
-            Deploy Now
-          </a>
-          <a
-            className={styles.secondary}
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+          </div>
+
+          <div className="control-group">
+            <label htmlFor="offsetY">Vertical displacement</label>
+            <input
+              type="range"
+              id="offsetY"
+              min="-100"
+              max="100"
+              value={offsetY}
+              onChange={(e) => setOffsetY(Number(e.target.value))}
+            />
+          </div>
+
+          <div className="control-group">
+            <label htmlFor="blur">Blur</label>
+            <input
+              type="range"
+              id="blur"
+              min="0"
+              max="100"
+              value={blur}
+              onChange={(e) => setBlur(Number(e.target.value))}
+            />
+          </div>
+
+          <div className="control-group">
+            <label htmlFor="spread">Spread</label>
+            <input
+              type="range"
+              id="spread"
+              min="-50"
+              max="50"
+              value={spread}
+              onChange={(e) => setSpread(Number(e.target.value))}
+            />
+          </div>
+
+          <div className="control-group">
+            <label htmlFor="color">Color</label>
+            <input
+              type="color"
+              id="color"
+              value={color}
+              onChange={(e) => setColor(e.target.value)}
+            />
+          </div>
+
+          <div className="control-group">
+            <label htmlFor="opacity">Opacity</label>
+            <input
+              type="range"
+              id="opacity"
+              min="0"
+              max="1"
+              step="0.01"
+              value={opacity}
+              onChange={(e) => setOpacity(Number(e.target.value))}
+            />
+          </div>
+
+          <div className="inline-group">
+            <input
+              type="checkbox"
+              id="inset"
+              checked={inset}
+              onChange={(e) => setInset(e.target.checked)}
+            />
+            <label htmlFor="inset">Inner Shadow</label>
+          </div>
+        </div>
+
+        <div className="result">
+          <div
+            className="output-box"
+            id="box"
+            style={{ boxShadow: boxShadowValue }}
+          />
+
+          <div className="code-output" id="code">
+            <div className="code-header">
+              <p>css</p>
+              <button id="copyBtn" onClick={copyToClipboard}>
+                {copied ? <FaCheck /> : <FaRegCopy />}
+                {copied ? "Copied!" : "Copy"}
+                </button>
+            </div>
+
+            <p className="code-line">
+              <span>box-shadow:</span>{" "}
+              {boxShadowValue};
+            </p>
+          </div>
         </div>
       </main>
     </div>
